70c7b75bac9d5c93318ea7d982a876387f821066
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-04-15 16:49:17 -0300

    Lista de Contatos + correçoes do chat

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,50 +5,84 @@ function loginUsuario() {
     const telaLogin = document.querySelector(".telaEntrada")
     telaLogin.classList.add("oculto")
     const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", {name: nomeDoUsuario})
-    promessa.then(buscarMensagem);
+    promessa.then(atualizarMensagens);
+    //promessa.then(removerTelaInicial)
     promessa.catch(tratarErroLogin);
+    setInterval(statusConexao, 5000)
+}
+function removerTelaInicial() {
+    const telaLogin = document.querySelector(".telaEntrada")
+    telaLogin.classList.add("oculto")
+}
+function atualizarMensagens() {
+    setInterval(buscarMensagem, 3000)
+}
+function tratarErroLogin() {
+    alert("Esse nome já existe");
+}
+function statusConexao() {
+    const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", {name: nomeDoUsuario})
+    promessa.catch(logout)
+}
+function logout(erro) {
+    statusErro = erro.response.status;
+    alert("Ocorreu erro"+ statusErro);
+    const telaLogin = document.querySelector(".telaEntrada")
+    telaLogin.classList.remove("oculto")
 }
 function buscarMensagem() {
     const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages")
     promessa.then(mensagensRecebidas)
 }
-function tratarErroLogin() {
-    alert("Esse nome já existe");
-}
-
 function mensagensRecebidas (resposta) {
     mensagens = resposta.data;
-    console.log(mensagens);
     chatDeMensagens()
 }
-
 function chatDeMensagens() {
     const todasAsMensagens = document.querySelector(".containerChat");
-    
+    todasAsMensagens.innerHTML = "";
     for(let i=0; i < mensagens.length ; i++) {
-        if(mensagens[i].type==='status') {
+        let mensagemDeStatus = mensagens[i].type==='status';
+        let mensagemReservada = mensagens[i].type==='private_message';
+        let mensagemGeral = mensagens[i].type==='message'
+        
+        if(mensagemDeStatus) {
             todasAsMensagens.innerHTML += ` 
             <div class="chat ${mensagens[i].type}">
                 (${mensagens[i].time}) ${mensagens[i].from} ${mensagens[i].text}
             </div>
             `
-        } else {
+        } else if(mensagemReservada && (mensagens[i].from===nomeDoUsuario || mensagens[i].to===nomeDoUsuario) ) {
+            todasAsMensagens.innerHTML += ` 
+            <div class="chat ${mensagens[i].type}">
+                (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
+            </div>
+            `
+        }else if(mensagemGeral){
             todasAsMensagens.innerHTML += ` 
             <div class="chat ${mensagens[i].type}">
                 (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
             </div>
             `
         }
-        
     }
+    window.scrollTo(0,todasAsMensagens.scrollHeight);
 }
-/**
+
+let tipoDeMensagem;
+function enviarMensagemPublica() {
+    tipoDeMensagem = "message"
+}
+function enviarMensagemPrivada() {
+    tipoDeMensagem = "private_message"
+}
 function enviarMensagem() {
     const msgDoUsuario = document.querySelector(".mensagem").value
-    //const novaMensagem = {from: nomeDoUsuario, to: , text: msgDoUsuario, type: tipoDeMensagem};
-    const promessa = axios.post("");
+    const novaMensagem = {from: nomeDoUsuario, to: destinatario, text: msgDoUsuario, type: tipoDeMensagem};
+    const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", novaMensagem);
     promessa.then(buscarMensagem);
     promessa.catch(tratarErro);
+    document.querySelector(".mensagem").value="";
 }
 function tratarErro() {
     alert();
@@ -56,41 +90,52 @@ function tratarErro() {
 
 let participantes;
 function buscarParticipantes() {
-    const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages")
+    const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants")
     promessa.then(participantesAtivos)
     promessa.catch(tratarErro)
 }
 function participantesAtivos(resposta) {
     participantes = resposta.data
+    const abaContatos = document.querySelector(".fundoEmbacado")
+    if(abaContatos.classList.contains("oculto")){
+        abaContatos.classList.remove("oculto")
+        console.log(participantes)
+        listaContatos()
+    } else {
+        abaContatos.classList.add("oculto")
+    }
+    
 }
 function listaContatos() {
-    const todosOsUsuarios = document.querySelector(".escolhaContato ul");
+    const todosOsUsuarios = document.querySelector("ul");
+    todosOsUsuarios.innerHTML = `
+    <li onclick="enviarPara(this)" id="Todos">
+        <div class="perfil">
+            <ion-icon name="people-sharp"></ion-icon> 
+            Todos
+        </div>
+        <ion-icon class="check" name="checkmark-outline"></ion-icon>
+    </li>
+    `;
     for(let i=0;i<participantes.length;i++) {
         todosOsUsuarios.innerHTML += `
-        <li>
+        <li onclick="enviarPara(this)" id="${participantes[i].name}">
             <div class="perfil">
                 <ion-icon name="people-sharp"></ion-icon> 
                 ${participantes[i].name}
             </div>
-            <ion-icon name="checkmark-outline"></ion-icon>
+            <ion-icon class="check" name="checkmark-outline"></ion-icon>
         </li>
         `;
     }
 }
-let tipoDeMensagem;
-function enviarMensagemPublica() {
-    tipoDeMensagem = "message"
-    alert("msg publica")
+let destinatario = '';
+function enviarPara(pessoa) {
+    destinatario = pessoa.id;
+    const selecionado = document.querySelector(".listaContatos .selecionado");
+    
+    if (selecionado!==null) {
+        selecionado.classList.remove("selecionado");
+    }
+    pessoa.classList.add("selecionado")
 }
-function enviarMensagemPrivada() {
-    tipoDeMensagem = "private_message"
-    alert("msg privada")
-}
-function statusConexao() {
-    axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", nomeDoUsuario)
-}
-function conexao() {
-    setInterval(statusConexao, 5000)
-}
-
-**/
