9a2311c80fd6030b113f6ec4fcbb559864e36aa4
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-04-15 23:40:13 -0300

    Ajustes de requisitos + Bonus

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,13 @@
 let nomeDoUsuario;
 let mensagens;
 function loginUsuario() {
+    const carregar = document.querySelector(".carregando");
+    carregar.classList.remove('oculto');
     nomeDoUsuario = document.querySelector(".nomeUsuario").value
-    const telaLogin = document.querySelector(".telaEntrada")
-    telaLogin.classList.add("oculto")
     const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", {name: nomeDoUsuario})
     promessa.then(atualizarMensagens);
-    //promessa.then(removerTelaInicial)
+    promessa.then(atualizarParticipantes)
+    promessa.then(removerTelaInicial)
     promessa.catch(tratarErroLogin);
     setInterval(statusConexao, 5000)
 }
@@ -16,6 +17,7 @@ function removerTelaInicial() {
 }
 function atualizarMensagens() {
     setInterval(buscarMensagem, 3000)
+    buscarMensagem()
 }
 function tratarErroLogin() {
     alert("Esse nome já existe");
@@ -27,8 +29,6 @@ function statusConexao() {
 function logout(erro) {
     statusErro = erro.response.status;
     alert("Ocorreu erro"+ statusErro);
-    const telaLogin = document.querySelector(".telaEntrada")
-    telaLogin.classList.remove("oculto")
 }
 function buscarMensagem() {
     const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages")
@@ -37,6 +37,7 @@ function buscarMensagem() {
 function mensagensRecebidas (resposta) {
     mensagens = resposta.data;
     chatDeMensagens()
+    
 }
 function chatDeMensagens() {
     const todasAsMensagens = document.querySelector(".containerChat");
@@ -48,34 +49,38 @@ function chatDeMensagens() {
         
         if(mensagemDeStatus) {
             todasAsMensagens.innerHTML += ` 
-            <div class="chat ${mensagens[i].type}">
+            <div class="chat status">
                 (${mensagens[i].time}) ${mensagens[i].from} ${mensagens[i].text}
             </div>
             `
         } else if(mensagemReservada && (mensagens[i].from===nomeDoUsuario || mensagens[i].to===nomeDoUsuario) ) {
             todasAsMensagens.innerHTML += ` 
-            <div class="chat ${mensagens[i].type}">
+            <div class="chat reservadamente">
                 (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
             </div>
             `
         }else if(mensagemGeral){
             todasAsMensagens.innerHTML += ` 
-            <div class="chat ${mensagens[i].type}">
+            <div class="chat publico">
                 (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
             </div>
             `
         }
     }
-    window.scrollTo(0,todasAsMensagens.scrollHeight);
+    let ultimaMensagem = document.querySelector(".containerChat div:last-of-type")
+    ultimaMensagem.scrollIntoView();
 }
 
-let tipoDeMensagem;
-function enviarMensagemPublica() {
-    tipoDeMensagem = "message"
-}
-function enviarMensagemPrivada() {
-    tipoDeMensagem = "private_message"
-}
+let tipoDeMensagem = 'message';
+function tipoDaMensagem(mensagemTipo) {
+    tipoDeMensagem = mensagemTipo.id;
+    const tipoSelecionado = document.querySelector(".visibilidade .selecionado");
+    if (tipoSelecionado!==null) {
+        tipoSelecionado.classList.remove("selecionado");
+    }
+    mensagemTipo.classList.add("selecionado")
+} 
+
 function enviarMensagem() {
     const msgDoUsuario = document.querySelector(".mensagem").value
     const novaMensagem = {from: nomeDoUsuario, to: destinatario, text: msgDoUsuario, type: tipoDeMensagem};
@@ -83,23 +88,44 @@ function enviarMensagem() {
     promessa.then(buscarMensagem);
     promessa.catch(tratarErro);
     document.querySelector(".mensagem").value="";
+    
 }
-function tratarErro() {
-    alert();
+function tratarErro(erro) {
+    statusErro = erro.resonse.status
+    alert(`("Ocorreu erro"+ statusErro)`)
+    window.location.reload()
+}
+document.querySelector(".nomeUsuario").addEventListener("keypress", loginComEnter)
+document.querySelector(".mensagem").addEventListener("keypress", mensagemComEnter)
+function loginComEnter(tecla) {
+    if(tecla.key === "Enter") {
+        loginUsuario()
+    }
+}
+function mensagemComEnter(tecla) {
+    if(tecla.key === "Enter") {
+        enviarMensagem()
+    }
 }
 
-let participantes;
+function atualizarParticipantes() {
+    setInterval(buscarParticipantes, 10000)
+    buscarParticipantes()
+}
+
 function buscarParticipantes() {
     const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants")
     promessa.then(participantesAtivos)
     promessa.catch(tratarErro)
 }
+let participantes;
 function participantesAtivos(resposta) {
     participantes = resposta.data
-    const abaContatos = document.querySelector(".fundoEmbacado")
+}
+function abrirAbaContatos() {
+    const abaContatos = document.querySelector(".contatosContainer")
     if(abaContatos.classList.contains("oculto")){
         abaContatos.classList.remove("oculto")
-        console.log(participantes)
         listaContatos()
     } else {
         abaContatos.classList.add("oculto")
@@ -107,7 +133,7 @@ function participantesAtivos(resposta) {
     
 }
 function listaContatos() {
-    const todosOsUsuarios = document.querySelector("ul");
+    const todosOsUsuarios = document.querySelector(".listaDeUsuarios");
     todosOsUsuarios.innerHTML = `
     <li onclick="enviarPara(this)" id="Todos">
         <div class="perfil">
@@ -129,13 +155,19 @@ function listaContatos() {
         `;
     }
 }
-let destinatario = '';
+let destinatario = 'Todos';
 function enviarPara(pessoa) {
     destinatario = pessoa.id;
-    const selecionado = document.querySelector(".listaContatos .selecionado");
+    const selecionado = document.querySelector(".listaDeUsuarios .selecionado");
     
     if (selecionado!==null) {
         selecionado.classList.remove("selecionado");
     }
     pessoa.classList.add("selecionado")
+    const infoDaMensagem = document.querySelector(".infoDestinatario")
+    if(tipoDeMensagem ==='private_message' && destinatario !=='Todos') {
+        infoDaMensagem.innerHTML=`Enviando para ${destinatario} (reservadamente)`
+    } else if(tipoDeMensagem ==='message') {
+        infoDaMensagem.innerHTML=`Enviando para ${destinatario} (público)`
+    }
 }
